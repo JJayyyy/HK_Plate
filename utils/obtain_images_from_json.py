3a5d3e66ac2b00beda821c209a3c9e2332967c0e
Jie <jie.zhao.joy@gmail.com>
2019-08-06 10:57:14 +0800

    Create obtain_images_from_json.py
    Obtain valid images from dataset package.

---
+++ utils/obtain_images_from_json.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+
+@File: obtain_images_from_json.py    
+@License: Copyright (c) 2018-2019 Siemens, Inc.
+@Author: Zhao Jie
+@Time: 8/6/2019 9:52 AM
+@Version: 1.0  
+@Description: Obtain valid images from annotation json file.
+
+"""
+
+# import lib
+import os
+import json
+import shutil
+import pathlib
+
+
+def obtain_valid_images(json_file, src, dst):
+    """
+    Copy the valid images to the target folder.
+    The paths of the valid images are recorded in .json file.
+    :param json_file (file)
+    :param src (folder)
+    :param dst (folder)
+    :return: list of images' paths
+    """
+    images_list = []
+    # If the dst dir does not exist, then create
+    pathlib.Path(dst).mkdir(parents=True, exist_ok=True)
+    # Load annotations
+    # VGG Image Annotator (up to version 2.0.8) saves each image in the form:
+    # "2019-04-25-0d0c6d1e-5812-4375-af41-e539b88d06cd.jpg12872": {
+    #     "filename": "2019-04-25-0d0c6d1e-5812-4375-af41-e539b88d06cd.jpg",
+    #     "size": "12872",
+    #     "regions": [
+    #         {
+    #             "shape_attributes": {
+    #                 "name": "polygon",
+    #                 "all_points_x": [...],
+    #                 "all_points_y": [...]
+    #             },
+    #             "region_attributes": {
+    #                 "class": "plate",
+    #                 "view": "slantview",
+    #                 "with_content": "No"
+    #             }
+    #         }
+    #     ],
+    #     "file_attributes": {}
+    # },
+    # We mostly care about the x and y coordinates of each region
+    # Note: In VIA 2.0, regions was changed from a dict to a list.
+    annotations = json.load(open(json_file))
+    annotations = list(annotations.values())  # don't need the dict keys
+
+    # The VIA tool saves images in the JSON even if they don't have any
+    # annotations. Skip unannotated images.
+    annotations = [a for a in annotations if a['regions']]
+
+    # Copy images from src folder to dst folder
+    for a in annotations:
+        image_path = os.path.join(src, a['filename'])
+        shutil.copy2(image_path, os.path.join(dst, a['filename']))
+        images_list.append(image_path)
+
+    return images_list
+
+
+if __name__ == '__main__':
+    valid_images = obtain_valid_images("../datasets/plate/valid/04/04/04.json", "../datasets/plate/original/04/04/", "../datasets/plate/valid/04/04/")
+    print("END")
